c8313411b29779e033dcc84660504928b0daca9f
Madhavi Peddireddy <madhavipeddireddysm@gmail.com>
2024-06-04 19:27:14 +0530

    Model trainer

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,11 @@ import os
 import pandas as pd
 import dill
 import sys
+from sklearn.preprocessing import LabelEncoder
+
 
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -18,3 +21,41 @@ def save_object(file_path, obj):
         
     except Exception as e:
         raise CustomException(e,sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            model.fit(X_train, y_train)
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+            train_model_score=r2_score(y_train, y_train_pred)
+            test_model_score=r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]]=test_model_score
+            
+        return report
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def preprocess_data(X_train, X_test):
+        try:
+            le = LabelEncoder()
+            # Identify categorical columns
+            cat_columns = [col for col in range(X_train.shape[1]) if isinstance(X_train[0, col], str)]
+            
+            for col in cat_columns:
+                all_values = list(X_train[:, col]) + list(X_test[:, col])
+                le.fit(all_values)
+                X_train[:, col] = le.transform(X_train[:, col])
+                X_test[:, col] = le.transform(X_test[:, col])
+
+            # Convert to float
+            X_train = X_train.astype(float)
+            X_test = X_test.astype(float)
+
+            return X_train, X_test
+        
+        except Exception as e:
+            raise CustomException(e, sys)
